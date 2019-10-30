ec9ef307362048f77d555c29cc30f2c226fa118a
Victor Cesar <victorcmggg@gmail.com>
2019-10-30 16:30:21 +0000

    first commit

---
+++ VictorPlatziFinanzas/Application/Authentication/OnBoarding/OnBoardingContainerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnBoardingContainerViewController.swift
+//  VictorPlatziFinanzas
+//
+//  Created by Victor Cesar on 24/10/19.
+//  Copyright © 2019 Victor Cesar. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VictorPlatziFinanzas/Extensions/Shadow.swift
@@ -0,0 +1,13 @@
+//
+//  Shadow.swift
+//  VictorPlatziFinanzas
+//
+//  Created by Victor Cesar on 24/10/19.
+//  Copyright © 2019 Victor Cesar. All rights reserved.
+//
+
+import UIKit
+
+class Shadow: NSObject {
+
+}
